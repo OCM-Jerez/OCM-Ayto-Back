a07ed8e7bbe6f1e2906548158f93a728663ce57d
Miguel Angel Martinez <correomam@gmail.com>
2021-05-08 14:31:25 +0200

    @InjectRepository

--- src/entities/programas/programas.service.ts
+++ src/entities/programas/programas.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { getManager, getRepository } from 'typeorm';
+import { getManager, getRepository, Repository } from 'typeorm';
 import { CreateProgramaDto } from './dto/create-programa.dto';
 import { UpdateProgramaDto } from './dto/update-programa.dto';
-import { ProgramaRepository } from './programa.repository';
 import { Programa } from './entities/programa.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class ProgramasService {
+  constructor(
+    @InjectRepository(Programa)
+    private readonly programaRepository: Repository<Programa>,
+  ) {}
+
   create(createProgramaDto: CreateProgramaDto) {
     return 'This action adds a new programa';
   }
@@ -16,8 +21,8 @@ export class ProgramasService {
     // const rawData = await entityManager.query(`SELECT * FROM PROGRAMA`);
     // return rawData;
 
-    const programaRepository = getRepository(Programa); // you can also get it via getConnection().getRepository() or getManager().getRepository()
-    const rawData = await programaRepository.find({
+    // const programaRepository = getRepository(Programa); // you can also get it via getConnection().getRepository() or getManager().getRepository()
+    const rawData = await this.programaRepository.find({
       order: {
         codPro: 'ASC',
       },
