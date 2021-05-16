c836c57a7a508cbbf7cfbbc7c7a9af5c48f972ca
Miguel Angel Martinez <correomam@gmail.com>
2021-05-16 09:46:05 +0200

    Limpio codigo.

--- src/common/http-exception.filter.ts
+++ src/common/http-exception.filter.ts
@@ -3,72 +3,21 @@ import {
   Catch,
   ArgumentsHost,
   HttpException,
-  HttpStatus,
   Logger,
 } from '@nestjs/common';
-import { Request, Response } from 'express';
-
-// @Catch(HttpException)
-// export class HttpExceptionFilter implements ExceptionFilter {
-//   catch(exception: HttpException, host: ArgumentsHost) {
-//     const ctx = host.switchToHttp();
-//     const response = ctx.getResponse<Response>();
-//     const request = ctx.getRequest<Request>();
-//     const status = exception.getStatus()
-//       ? exception.getStatus()
-//       : HttpStatus.INTERNAL_SERVER_ERROR;
-
-//     const errorResponse = {
-//       code: status,
-//       timestamp: new Date().toLocaleDateString(),
-//       path: request.url,
-//       method: request.method,
-//       message:
-//         status !== HttpStatus.INTERNAL_SERVER_ERROR
-//           ? exception.message || exception.message || null
-//           : 'Internal server error',
-//     };
-
-//     if (status === HttpStatus.INTERNAL_SERVER_ERROR) {
-//       Logger.error(
-//         `${request.method} ${request.url}`,
-//         exception.stack,
-//         'ExceptionFilter',
-//       );
-//     } else {
-//       Logger.error(
-//         `${request.method} ${request.url}`,
-//         JSON.stringify(errorResponse),
-//         'ExceptionFilter',
-//       );
-//     }
-
-//     // response.status(status).json(errorResponse);
-//   }
-
+import { Request } from 'express';
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
-    // const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
-    // const status = exception.getStatus();
-    // const message = exception.getResponse();
 
     const errorResponse = {
-      // code: exception.getStatus(),
-      // timestamp: new Date().toLocaleDateString(),
       method: request.method,
       path: request.url,
       response: exception.getResponse(),
     };
 
-    Logger.error(
-      // `${request.method} ${request.url}`,
-      JSON.stringify(errorResponse),
-      '',
-      'ExceptionFilter',
-      false,
-    );
+    Logger.error(JSON.stringify(errorResponse), '', 'ExceptionFilter', false);
   }
 }
