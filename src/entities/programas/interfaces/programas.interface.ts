0b170d850abe5f931535ceaddf9f13237a6dd6d1
mamjerez <correomam@gmail.com>
2021-12-07 21:20:39 +0100

    Add Interface

---
+++ src/entities/programas/interfaces/programas.interface.ts
@@ -0,0 +1,12 @@
+export interface IResponsePrograma {
+    id: number;
+    codPro: string;
+    descripcionAyto?: any;
+    descripcionOCM: string;
+    WebOCM?: any;
+    proCreatedDate?: any;
+    proDeletedDate?: any;
+    uso?: any;
+    observaciones?: any;
+    codOrg?: any;
+}
