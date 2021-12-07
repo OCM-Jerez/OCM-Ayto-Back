1e12ab3eda857e73571debdfe107a5cf1f3d46e5
mamjerez <correomam@gmail.com>
2021-12-07 21:20:05 +0100

    Delete async-await No es necesario

--- src/entities/programas/programas.service.ts
+++ src/entities/programas/programas.service.ts
@@ -5,33 +5,34 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { CreateProgramaDto } from './dto/create-programa.dto';
 import { UpdateProgramaDto } from './dto/update-programa.dto';
 import { Programa } from './entities/programa.entity';
+import { IResponsePrograma } from './interfaces/programas.interface';
 
 @Injectable()
 export class ProgramasService {
   constructor(
     @InjectRepository(Programa)
     private readonly programaRepository: Repository<Programa>,
-  ) {}
+  ) { }
 
-  async create(payload: CreateProgramaDto): Promise<Programa> {
+  create(payload: CreateProgramaDto): Promise<Programa> {
     return this.programaRepository.save(payload);
   }
 
   // En el ejemplo de Nest tipa las respuestas como Promise.
   // https://github.com/nestjs/nest/blob/f472852d441b7f66e52449e09f885ab57807bf96/sample/05-sql-typeorm/src/photo/photo.controller.ts
-  async findAll(): Promise<void | Programa[]> {
-    return await this.programaRepository.find({ order: { codPro: 'ASC' } });
+  findAll(): Promise<Programa[]> {
+    return this.programaRepository.find({ order: { codPro: 'ASC' } });
   }
 
-  async findOne(id: string) {
-    return await this.programaRepository.findOne(id);
+  findOne(id: string) {
+    return this.programaRepository.findOne(id);
   }
 
-  async update(id: string, updateProgramaDto: UpdateProgramaDto) {
-    return await this.programaRepository.update(id, updateProgramaDto);
+  update(id: string, updateProgramaDto: UpdateProgramaDto) {
+    return this.programaRepository.update(id, updateProgramaDto);
   }
 
-  async delete(id: string): Promise<any> {
-    return await this.programaRepository.delete(id);
+  delete(id: string): Promise<any> {
+    return this.programaRepository.delete(id);
   }
 }
