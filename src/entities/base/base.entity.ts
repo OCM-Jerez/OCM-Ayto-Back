c776d58c3b0111cf7a5cc58d87368db5b2e56152
mamjerez <correomam@gmail.com>
2021-12-07 21:19:20 +0100

    Use {select: false}

--- src/entities/base/base.entity.ts
+++ src/entities/base/base.entity.ts
@@ -7,22 +7,25 @@ import {
 } from 'typeorm';
 // import { ApiModelProperty } from '@nestjs/swagger';
 
+// https://typeorm.io/#/select-query-builder/hidden-columns
+// Con select: false se evita quel el campo se incluya en la respuesta de una consulta.
+
 export abstract class BaseEntity {
   @ObjectIdColumn()
   @PrimaryGeneratedColumn('uuid')
   // @ApiModelProperty({ example: '66ed8031-d40e-4f30-8ae4-8bf5c2ca87ff', description: 'Entity id' })
   id: string;
 
-  @Column({ type: 'varchar', length: 36, default: 'mam', nullable: true })
+  @Column({ type: 'varchar', length: 36, default: 'mam', nullable: true, select: false })
   createdBy: string;
 
   // https://typeorm.io/#/decorator-reference/createdatecolumn
-  @CreateDateColumn()
+  @CreateDateColumn({ select: false })
   createdDate: Date;
 
-  @Column({ type: 'varchar', length: 36, default: 'mam', nullable: true })
+  @Column({ type: 'varchar', length: 36, default: 'mam', nullable: true, select: false })
   lastUpdatedBy: string;
 
-  @UpdateDateColumn()
+  @UpdateDateColumn({ select: false })
   lastUpdatedDate: Date;
 }
