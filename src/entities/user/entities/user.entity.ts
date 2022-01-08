ad295c4c69b30ea296cae09ec4f6e2ab0d0ccbe4
mamjerez <correomam@gmail.com>
2022-01-08 09:09:27 +0100

    Change length to 20 & nullable to false

--- src/entities/user/entities/user.entity.ts
+++ src/entities/user/entities/user.entity.ts
@@ -3,10 +3,10 @@ import { BaseEntity } from '../../base/base.entity';
 
 @Entity('user')
 export class User extends BaseEntity {
-    @Column({ type: 'varchar', length: 255, nullable: true })
+    @Column({ type: 'varchar', length: 20, nullable: false })
     login: string;
 
-    @Column({ type: 'varchar', length: 255, nullable: true })
+    @Column({ type: 'varchar', length: 20, nullable: false })
     password: string;
 
 }
