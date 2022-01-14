aa4d13789da1d0069004b6634b8253a350724c7d
mamjerez <correomam@gmail.com>
2022-01-14 06:32:31 +0100

    Clean code

--- src/auth/login/login.service.ts
+++ src/auth/login/login.service.ts
@@ -1,33 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import { CreateLoginDto } from './dto/create-login.dto';
-import { UpdateLoginDto } from './dto/update-login.dto';
-import { UserRepository } from '../../entities/user/user.repository';
+import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-import { compare } from 'bcryptjs';
 
+import { User } from 'src/entities/user/entities';
+
+import { compare } from 'bcryptjs';
 @Injectable()
 export class LoginService {
-  constructor(@InjectRepository(UserRepository) private userRepository: UserRepository) { }
-
-  async findOne(login: string, password: string): Promise<boolean> {
-    return (await this.userRepository.findOne({ where: { login: login, email: password } }) ? true : false);
-  }
-
-  // async validateUser(login: string, pass: string): Promise<any> {
-  //   const user = await this.userRepository.findOne({ login });
-  //   console.log('user', user);
-
-  //   if (user && (await compare(pass, user.password))) {
-  //     const { password, ...rest } = user;
-  //     return rest;
-  //   }
-  //   return null;
-  // }
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>
+  ) { }
 
   async validateUser(login: string, pass: string): Promise<any> {
     const user = await this.userRepository.findOne({ login });
     return ((user && (await compare(pass, user.password))) ? true : false)
   }
 
+  // async validateUser(login: string, password: string): Promise<any> {
+  //   // faltaria decodificar password
+  //   return (await this.userRepository.findOne({ where: { login: login, password: password } }) ? true : false);
+  // }
 
 }
